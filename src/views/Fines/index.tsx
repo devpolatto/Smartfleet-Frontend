c6c0d00a4c8b8e0b7e0fc5d7b1791a7d68245fbb
Angelo Polatto <polatto.xcode.job@gmail.com>
2022-12-14 21:13:05 -0300

    add material ui table

--- src/views/Fines/index.tsx
+++ src/views/Fines/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import CustomPaginationActionsTable from '../../components/TableHistory'
+
 const Fines: React.FC = () => {
   return (
     <div className='grid-content-page'>
-      Multas
+      <CustomPaginationActionsTable/>
     </div>
   )
 }
