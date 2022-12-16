d62863b5b9639860f4272eacafc18e9c83b15e2b
Stephane Medeiros <taina_42@msn.com>
2022-12-16 17:09:11 -0300

    CRIAÇÃO E ORGANIZAÇÃO DO SIDENAV - ICONES

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
 
 const Header: React.FC = () => {
   return(
-     <div className='grid-header'>
-      Header
+     <div className='grid-header  font-sans  font-semibold  bg-background-primary '>
+      
     </div>
   );
 }
