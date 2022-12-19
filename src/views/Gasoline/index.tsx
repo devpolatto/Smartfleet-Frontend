a6f9110fb985612cdcf26a6fd32f3e1ee92b141a
Angelo Polatto <polatto.xcode.job@gmail.com>
2022-12-19 17:59:24 -0300

    :bug: correcting the routes structure and completing the rest of the views to visualize in the henderizations

---
+++ src/views/Gasoline/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Deashboard: React.FC = () => {
+  return (
+    <div className='grid-content-page'>
+      Gasolina
+    </div>
+  );
+}
+
+export default Deashboard
