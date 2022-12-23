0a3e49fa0082558369b5244c21184500b98d4b7a
Stephane Medeiros <taina_42@msn.com>
2022-12-23 12:12:07 -0300

    header reajustado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,24 +3,27 @@ import AddIcon from '@mui/icons-material/Add';
 import AnnouncementRoundedIcon from '@mui/icons-material/AnnouncementRounded';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
+
 const Header: React.FC = () => {
   return(
-     <div className='grid-header font-sans  bg-background-primary '>
+     <div className='grid-header font-sans bg-background-primary flex justify-between items-center border-spacing-1 px-4'>
 
-    <div className='flex flex-wrap justify-end '>
-    <div className=' mt-4 mr-6 w-[25px] h-[23px] text-red-300   '>
-    <AnnouncementRoundedIcon sx={{ fontSize: 24 }}/> 
-    
-    </div>    
-    
-   <div className=' mt-4 mr-4 w-[25px] h-[23px] text-blue-500  '>
-   <AccountCircleRoundedIcon sx={{ fontSize: 24 }}/>
-   </div>
-   </div>
-  </div>
+        <div>
+          <div className='flex flex-wrap justify-end '>
+          </div>  
+        </div>
 
-
+          <div className='flex flex-row h-full items-center gap-10'>
+            <div className='text-red-300'>   
+              <AnnouncementRoundedIcon sx={{ fontSize: 24 }}/> 
+            </div>    
+              
+            <div className='text-blue-500  '>
+              <AccountCircleRoundedIcon sx={{ fontSize: 24 }}/>
+              {/* </div> */}
+            </div>
+        </div>
+    </div>
     );
 }
-
 export default Header; 
