91573a29b860b0513a6d1f57a39610cd9611b64f
Angelo Polatto <polatto.xcode.job@gmail.com>
2023-01-03 23:19:08 -0300

    add new theme

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -7,58 +7,67 @@ import AttachMoneyRoundedIcon from '@mui/icons-material/AttachMoneyRounded';
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import AnnouncementRoundedIcon from '@mui/icons-material/AnnouncementRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import Icon from '@mui/material/Icon'
 
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 const links = [
   {
     id: 1,
-    path: '/',
+    path: '',
     icon: <DashboardRoundedIcon sx={{ fontSize: 24  }}/>,
     text: 'Dashboard'
   },
   {
     id: 2,
-    path: '/carros',
+    path: 'carros',
     icon: <DriveEtaRoundedIcon sx={{ fontSize: 24  }}/>,
     text: 'Carros'
   },
   {
     id: 3,
-    path: '/gasolina',
+    path: 'gasolina',
     icon: <LocalGasStationRoundedIcon sx={{ fontSize: 24  }}/>,
     text: 'Gasolina'
   },
   {
     id: 4,
-    path: '/multas',
+    path: 'multas',
     icon: <RateReviewRoundedIcon sx={{ fontSize: 24  }}/>,
     text: 'Multas'
   },
   {
     id: 5,
-    path: '/financeiro',
+    path: 'financeiro',
     icon: <AttachMoneyRoundedIcon sx={{ fontSize: 24  }}/>,
     text: 'Financeiro'
   },
   {
     id: 6,
-    path: '/motorista',
+    path: 'motorista',
     icon: <BadgeRoundedIcon sx={{ fontSize: 24  }}/>,
     text: 'Motorista'
   },
 ]
 
 const SideNav: React.FC = () => {
+
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+  // get the current route and perform the active link exchange in the menus
+
   return(
-    <div className='grid-sidenav py-4 flex flex-col justify-between bg-gradient-to-b from-[#29238C] to-[#46429D]'>
+    <div className='grid-sidenav py-4 flex flex-col justify-between bg-sidenav'>
 
       <div className=' flex flex-col items-center gap-2 top-14'>
         {links.map((link, index) => {
           return (
-            <Link to={link.path} key={index}>
-              <div className='flex flex-col items-center w-[68px] h-[62px]  text-white justify-center '>
-                {link.icon}
+            <Link to={`/${link.path}`} key={index}>
+              <div className='flex flex-col items-center gap-1 w-[68px] h-[62px] text-white justify-center hover:bg-slate-700 duration-300 rounded-md'>
+                <Icon sx={splitLocation[1] === link.path ? {color: '#1976d2'} : {}}>
+                  {link.icon}
+                </Icon>
                 <p className=' leading-4 text-[10px]'>{link.text}</p>
               </div>
             </Link>
@@ -66,7 +75,7 @@ const SideNav: React.FC = () => {
         })}
       </div>
       
-      <div className='flex flex-col items-center w-[68px] h-[62px]  text-white justify-center'> 
+      <div className='flex flex-col items-center w-[68px] h-[62px]  text-white justify-center hover:bg-slate-700 duration-300 rounded-md'> 
         <SettingsRoundedIcon sx={{ fontSize: 24 }}/>
         <p className= 'text-[10px]'></p>
       </div> 
